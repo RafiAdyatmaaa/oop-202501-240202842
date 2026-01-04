d0efe385f286969ec05bbfaf4fb87d3bd4ae44b4
RafiAdyatmaaa <rafiadyatmamuhammad5@gmail.com>
2026-01-04 23:04:09 +0700

    Implement main method for shopping cart functionality

--- praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/maincart.java
+++ praktikum/week7-koleksi-keranjang/src/main/java/com/upb/agripos/maincart.java
@@ -1,1 +1,21 @@
+package com.upb.agripos;
 
+
+public class Maincart {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Muhammad Rafi Adyatma-240202842 (Week7)");
+
+        Product p1 = new Product("P01", "Beras", 50000, 0);
+        Product p2 = new Product("P02", "Pupuk", 30000, 0);
+        
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addProduct(p1);
+        cart.addProduct(p2);
+        cart.printCart();
+
+    System.out.println("\nSetelah menghapus " + p1.getCode() + " " + p1.getName() + " dari keranjang:");
+        cart.removeProduct(p1);
+        cart.printCart();
+    }
+}
